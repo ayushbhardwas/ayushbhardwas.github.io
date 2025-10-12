229a284517a5767369483b54215d7ea8e0822603
Ayush <ayush10.bhardwas@gmail.com>
2025-10-12 18:31:51 +0200

    [ADD] added basic html, css, and javascript to setup and test

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("testButton");
+  button.addEventListener("click", () => {
+    alert("Your GitHub Pages site is working!");
+  });
+});
